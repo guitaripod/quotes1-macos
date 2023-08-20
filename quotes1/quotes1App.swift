ea7e7800bbbc7a87418ede76b43a0a84c3834419
marcusziade <marcus.ziade@ouraring.com>
2023-08-20 09:27:48 +0300

    Initial Commit

---
+++ quotes1/quotes1App.swift
@@ -0,0 +1,17 @@
+//
+//  quotes1App.swift
+//  quotes1
+//
+//  Created by Marcus Ziadé on 20.8.2023.
+//
+
+import SwiftUI
+
+@main
+struct quotes1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
